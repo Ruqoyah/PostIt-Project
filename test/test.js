878f2d86b4ffa35f078631fdde77d344d74e89b0
Ruqoyah <rukayatodukoya123@gmail.com>
2017-07-15 13:58:50 +0100

    Writing more test and made some correction

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
 import supertest from 'supertest';
 import chai from 'chai';
-import app from './server';
+import server from '../app';
+
 
 const expect = chai.expect;
-const requestHandler = supertest(app);
+const requestHandler = supertest(server);
 
 const testUser = {
   username: 'ruqoyah',
@@ -12,11 +13,6 @@ const testUser = {
   email: 'ruqoyah@gmail.com',
   password: 'oriyomi',
   cpassword: 'oriyomi'
-};
-
-const testGroup = {
-  groupname: 'codespire',
-  groupdescription: 'aiming beyond code',
 };
 
 describe('User', () => {
@@ -29,8 +25,10 @@ describe('User', () => {
       .end((err, res) => {
         expect(res.body.status).to.equal('success');
         expect(res.body.data.user.email).to.equal(testUser.email);
+      });
+  });
 
-    it('should not create same username twice', (done) => {
+  it('should not create same username twice', (done) => {
     requestHandler.post('/api/user/signup')
       .set('Accept', 'application/json')
       .send(testUser)
@@ -79,21 +77,24 @@ describe('User', () => {
       });
   });
 });
-  describe('Group', () => {
+describe('Group', () => {
   it('should create a group', (done) => {
     // Create group
     requestHandler.post('/api/group')
       .set('Accept', 'application/json')
-      .send(testGroup)
+      .send(testUser)
       .expect(201)
       .end((err, res) => {
         expect(res.body.status).to.equal('success');
-        expect(res.body.data.user.groupname).to.equal(testGroup.groupname);
+        expect(res.body.message).to.equal('Group successfully created');
+        done();
+      });
+  });
 
   it('should fail if group name is not provided', (done) => {
     requestHandler.post('/api/group')
       .set('Accept', 'application/json')
-      .send(testGroup)
+      .send(testUser)
       .expect(400)
       .end((err, res) => {
         expect(res.body.status).to.equal('fail');
@@ -101,10 +102,10 @@ describe('User', () => {
         done();
       });
   });
-   it('should not create the same group name twice', (done) => {
+  it('should not create the same group name twice', (done) => {
     requestHandler.post('/api/group')
       .set('Accept', 'application/json')
-      .send(testGroup)
+      .send(testUser)
       .expect(400)
       .end((err, res) => {
         expect(res.body.status).to.equal('fail');
@@ -129,7 +130,7 @@ describe('User', () => {
   it('should fail if username is not provided', (done) => {
     requestHandler.post('/api/group/:groupId/user')
       .set('Accept', 'application/json')
-      .send(testGroup)
+      .send(testUser)
       .expect(401)
       .end((err, res) => {
         expect(res.body.status).to.equal('fail');
@@ -140,23 +141,11 @@ describe('User', () => {
   it('should fail if user is already in the group', (done) => {
     requestHandler.post('/api/group/:groupId/user')
       .set('Accept', 'application/json')
-      .send(testGroup)
+      .send(testUser)
       .expect(400)
       .end((err, res) => {
         expect(res.body.status).to.equal('fail');
         expect(res.body.message[0]).to.equal('User is already a group user');
-        done();
-      });
-  });
- it('should be able to get messages from a group', (done) => {
-   // Get messsages from group
-    requestHandler.get('/api/group/:groupId/messages')
-      .set('Accept', 'application/json')
-      .expect(201)
-      .end((err, res) => {
-        expect(res.body.data.userId).to.equal(userId);
-        expect(res.body.data.userGroup).to.equal(userGroup);
-        expect(res.body.data.message).to.equal(message);
         done();
       });
   });
